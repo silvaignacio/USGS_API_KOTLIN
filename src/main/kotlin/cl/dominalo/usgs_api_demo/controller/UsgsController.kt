7187480856d54bcd18958df3a2777de1e35d8061
Ignacio Silva Silva <isilva@seiza-ti.cl>
2021-03-29 13:25:45 -0300

    Fix corigin

--- src/main/kotlin/cl/dominalo/usgs_api_demo/controller/UsgsController.kt
+++ src/main/kotlin/cl/dominalo/usgs_api_demo/controller/UsgsController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
+@CrossOrigin
 @RequestMapping("/terremoto")
 open class UsgsController(@Autowired private val terremotoService: UsgsService) {
 
