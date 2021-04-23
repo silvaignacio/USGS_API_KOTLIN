9f9950c16e4e8a632729b9866ed08faa7027075e
Ignacio Silva <isilva@seiza-ti.cl>
2021-04-23 15:25:00 -0400

    Fix

--- src/main/kotlin/cl/dominalo/usgs_api_demo/model/Eartquake.kt
+++ src/main/kotlin/cl/dominalo/usgs_api_demo/model/Eartquake.kt
@@ -1,3 +1,7 @@
 package cl.dominalo.usgs_api_demo.model
 
-data class Eartquake(val title: String?, val mag: Long?, val place: String?, val coordinates: List<Double?>)
+data class Eartquake(val title: String?,
+                     val mag: Long?,
+                     val place: String?,
+                     val coordinates: List<Double?>,
+                     val radius: Long = 3616)
